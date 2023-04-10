c9398ea28c09e8e281f67017d39d8c8b5ebcbc66
destu3 <obose03@gmail.com>
2023-04-10 19:12:34 +0100

    add simple error handling middleware

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
-import express from "express";
-import morgan from "morgan";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+import express from 'express';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 // import routers
-import appointmentRouter from "./routers/appointmentRouter.js";
-import categoryRouter from "./routers/categoryRouter.js";
-import facilityRouter from "./routers/facilityRouter.js";
-import healthProfileRouter from "./routers/healthProfileRouter.js";
-import prescriptionRouter from "./routers/prescriptionRouter.js";
-import serviceRouter from "./routers/serviceRouter.js";
-import userRouter from "./routers/userRouter.js";
-import AuthController from "./controllers/AuthController.js";
+import appointmentRouter from './routers/appointmentRouter.js';
+import categoryRouter from './routers/categoryRouter.js';
+import facilityRouter from './routers/facilityRouter.js';
+import healthProfileRouter from './routers/healthProfileRouter.js';
+import prescriptionRouter from './routers/prescriptionRouter.js';
+import serviceRouter from './routers/serviceRouter.js';
+import userRouter from './routers/userRouter.js';
+import AuthController from './controllers/AuthController.js';
+import errorHandler from './helpers/errorHandler.js';
 
 const app = express();
 
@@ -25,21 +26,23 @@ app.use(express.json());
 app.use(cookieParser());
 
 // log request info
-if (process.env.APP_ENV !== "production") app.use(morgan("dev"));
+if (process.env.APP_ENV !== 'production') app.use(morgan('dev'));
 
-app.get("/api/", (req, res, next) => {
-  res.status(200).json("Root API route");
+app.get('/api/', (req, res, next) => {
+  res.status(200).json('Root API route');
 });
 
 // mount routers
-app.use("/api/users", userRouter);
+app.use('/api/users', userRouter);
 // all routes should be protected
 app.use(new AuthController().protect());
-app.use("/api/appointments", appointmentRouter);
-app.use("/api/categories", categoryRouter);
-app.use("/api/facilities", facilityRouter);
-app.use("/api/healthProfiles", healthProfileRouter);
-app.use("/api/prescriptions", prescriptionRouter);
-app.use("/api/services", serviceRouter);
+app.use('/api/appointments', appointmentRouter);
+app.use('/api/categories', categoryRouter);
+app.use('/api/facilities', facilityRouter);
+app.use('/api/healthProfiles', healthProfileRouter);
+app.use('/api/prescriptions', prescriptionRouter);
+app.use('/api/services', serviceRouter);
+
+app.use(errorHandler);
 
 export default app;
